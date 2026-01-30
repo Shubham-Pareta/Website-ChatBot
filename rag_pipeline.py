fc1f1510ad9a328de173fd4a6dd3f209aaec9d1f
Shubham-Pareta <ishubhampareta@gmail.com>
2026-01-30 11:36:28 +0530

    Updated code for Streamlit Cloud deployment

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,7 +1,7 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.chat_models import ChatGroq
+from langchain_groq import ChatGroq
 import os
 
 embedding_model = HuggingFaceEmbeddings(
